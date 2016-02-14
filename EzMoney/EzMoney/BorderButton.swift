fd08177364891fda4d4bd910be52a3cab161c1b3
Jonathan Velazquez <jonhysiker@icloud.com>
2016-02-13 18:34:04 -0600

    Storyboard: Login & Signup -> Graphic elements

--- EzMoney/EzMoney/BorderButton.swift
+++ EzMoney/EzMoney/BorderButton.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import UIKit
+import Spring
+
+@IBDesignable
+class BorderButton: SpringButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+    
+    @IBInspectable var color: UIColor = UIColor.clearColor() {
+        didSet{
+            setupView()
+        }
+    }
+    
+    @IBInspectable var width: CGFloat = 1 {
+        didSet{
+            setupView()
+        }
+    }
+    
+    @IBInspectable var radius: CGFloat = 0 {
+        didSet{
+            setupView()
+        }
+    }
+    
+    func setupView(){
+        
+        self.layer.borderColor = color.CGColor
+        self.layer.borderWidth = width
+        self.layer.cornerRadius = radius
+        
+        
+    }
+    
+    
+}
