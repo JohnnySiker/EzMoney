645896e4816ba44e5d92fc9d4f3b9b418799034c
Jonathan Velazquez <jonhysiker@icloud.com>
2016-02-13 20:08:30 -0600

    Added Login & Signup Controllers

---
+++ EzMoney/LoginController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginController.swift
+//  EzMoney
+//
+//  Created by Jonathan Velazquez on 13/02/16.
+//  Copyright © 2016 Jonathan Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ EzMoney/SignupController.swift
@@ -0,0 +1,17 @@
+//
+//  SignupController.swift
+//  EzMoney
+//
+//  Created by Jonathan Velazquez on 13/02/16.
+//  Copyright © 2016 Jonathan Velazquez. All rights reserved.
+//
+
+import UIKit
+
+class SignupController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
